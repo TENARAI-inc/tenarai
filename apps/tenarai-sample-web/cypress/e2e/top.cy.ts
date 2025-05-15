21be090660436d8dc6a26692f7710411307635ad
toshick <toshickjazz@mac.com>
2025-05-15 22:21:23 +0900

    fix: only

--- apps/tenarai-sample-web/cypress/e2e/top.cy.ts
+++ apps/tenarai-sample-web/cypress/e2e/top.cy.ts
@@ -1,9 +1,9 @@
 describe('top', () => {
-  describe('render (no login)', { testIsolation: false }, () => {
+  describe.only('render (no login)', { testIsolation: false }, () => {
     before(() => {
       cy.visit('/');
     });
-    it.only('render header', () => {
+    it('render header', () => {
       cy.get('header').within(() => {
         cy.contains('Tenarai Sample Web');
         cy.contains('a', 'トップ');
