61612a8e3b32a8c7d2c0f35bfe3bcd341bf3e03b
toshick <toshickjazz@mac.com>
2025-05-17 17:22:03 +0900

    feat: cypress videos

--- apps/tenarai-sample-web/cypress.config.js
+++ apps/tenarai-sample-web/cypress.config.js
@@ -1,11 +1,26 @@
+import fs from 'fs';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
   viewportWidth: 1200,
+  videoCompression: true,
+  videosFolder: 'cypress/videos',
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('after:spec', (_spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === 'failed')
+          );
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video);
+          }
+        }
+      });
     },
+    video: process.env.CYPRESS_CI === 'true',
   },
 });
