8f327037a631ba2fabe669731a05207e3451b347
toshick <toshickjazz@mac.com>
2025-05-27 23:55:31 +0900

    feat: week01 1

---
+++ apps/tenarai-sample-web/pages/sample.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const title = ref('Sample Page');
+
+setTimeout(() => {
+  title.value = 'Updated Sample Page Title';
+}, 3000);
+</script>
+
+<style scoped lang="css">
+div {
+  padding: 40px;
+}
+</style>
