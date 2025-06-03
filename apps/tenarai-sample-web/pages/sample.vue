f6d40101cecfa2db51ea12d63ac6b3d8106deab3
toshick <toshickjazz@mac.com>
2025-06-03 23:38:14 +0900

    feat: week01-2

--- apps/tenarai-sample-web/pages/sample.vue
+++ apps/tenarai-sample-web/pages/sample.vue
@@ -7,6 +7,14 @@
 <script setup lang="ts">
 const title = ref('Sample Page');
 
+const cats = [
+  { name: 'ねこすけ', age: 3, type: 'マンチカン' },
+  { name: 'にゃんた', age: 1, type: 'ロシアンブルー' },
+  { name: 'ニャース', age: 5, type: 'ペルシャ' },
+];
+
+console.log(JSON.stringify(cats, null, 2));
+
 setTimeout(() => {
   title.value = 'Updated Sample Page Title';
 }, 3000);
